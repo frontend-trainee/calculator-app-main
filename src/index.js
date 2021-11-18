023b79e953e50c35eba366a7ba165a698a447b43
MleMoe <i@mle.moe>
2021-11-18 23:16:27 +0800

    :sparkles: add micro app config

--- src/index.js
+++ src/index.js
@@ -1,10 +1,53 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App'
-import './index.css'
+import "./public-path";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
 
+import { HashRouter as Router } from "react-router-dom";
 
-ReactDOM.render(
-    <App />,
-    document.getElementById('root')
-);
+// ReactDOM.render(<App />, document.getElementById("root"));
+
+function render(props) {
+  const { container } = props;
+  ReactDOM.render(
+    <React.StrictMode>
+      <Router
+        basename={
+          window.__POWERED_BY_QIANKUN__
+            ? process.env.REACT_APP_MICRO_ROUTER
+            : "/"
+        }
+      >
+        <App />
+      </Router>
+    </React.StrictMode>,
+    container
+      ? container.querySelector("#root")
+      : document.querySelector("#root")
+  );
+}
+
+if (!window.__POWERED_BY_QIANKUN__) {
+  render({});
+}
+
+export async function bootstrap() {
+  console.log("[react16] react app bootstraped");
+}
+
+export async function mount(props) {
+  console.log("[react16] props from main framework", props);
+  render(props);
+}
+
+export async function unmount(props) {
+  const { container } = props;
+  ReactDOM.unmountComponentAtNode(
+    container
+      ? container.querySelector("#root")
+      : document.querySelector("#root")
+  );
+}
+reportWebVitals();
